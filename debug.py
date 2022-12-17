1da2ecef698f71b72096c855e2c9dc314a5ba115
Ben Brenner <ben.brenner@gmail.com>
2022-12-17 11:15:11 -0600

    ep 7 Simultaneity Principle jaqueline kelm

--- debug.py
+++ debug.py
@@ -2,5 +2,5 @@ import os
 os.system("python freeze.py")
 os.system("git status")
 os.system("git add -A")
-os.system('git commit -m "ep 6 weekly visioning jaqualine kelm"' )
+os.system('git commit -m "ep 7 Simultaneity Principle jaqueline kelm"' )
 os.system("git push")
